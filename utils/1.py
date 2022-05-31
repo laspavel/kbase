b1f7a9ea4ffd4192c3d319c78f42e909ca7ca1f8
laspavel <laspavel@gmail.com>
2022-05-31 17:39:03 +0300

    Add telegram send snippet

---
+++ utils/1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+tree_str = ''
+
+def generate_tree(pathname, n=0):
+    global tree_str
+    if pathname.is_file():
+        if pathname.name.endswith(".md"):
+           tree_str += '    |' * n + '-' * 4 + pathname.name + '\n'
+    elif pathname.is_dir():
+        tree_str += '    |' * n + '-' * 4 + \
+            str(pathname.relative_to(pathname.parent)) + '\\' + '\n'
+        for cp in pathname.iterdir():
+            generate_tree(cp, n + 1)
+
+if __name__ == '__main__':
+    generate_tree(Path.cwd().parent)
+    print(tree_str)
--- utils/update_context.py
+++ utils/update_context.py
@@ -2,27 +2,38 @@
 
 import glob, os
 
-
 os.chdir('../')
 file_list=[]
 for root, dirs, files in os.walk("."):
     for file in files:
         if file.endswith(".md"):
-             if os.path.join(root, file)!='../README.md':
+             if os.path.join(root, file)!='./README.md':
                  file_list.append(os.path.join(root, file))
 
-contents={}
+ds={}
 for file_l in file_list:
    temp = open(file_l, "r").readlines() 
    for i in temp:
        if i.startswith('# '):
-           i=i.replace('# ', '').replace(' #', '')
+           i=i.replace('# ', '').replace(' #', '').rstrip()
            file_l=file_l.replace('./','')
-           if file_l in contents:
-               contents[file_l].append(i)
+           if file_l in ds:
+               ds[file_l].append(i)
            else:
-               contents[file_l]=[i]
+               ds[file_l]=[i]
+
+contents={}
+for d in ds:
+    x=d.split('/')
+    del x[0]
+    del x[len(x)-1]
+
+    for y in x:
+        if y in contents:
+            contens[y].extend({'name': d)
 
 
-print(contents)
+    print(x)
+
+
 exit(0)
