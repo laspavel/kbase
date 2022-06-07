9ea733956088b1e7b5344dbbd6d3baf1f0d1e924
laspavel <laspavel@gmail.com>
2022-06-07 21:28:40 +0300

    Update 1.py

--- utils/1.py
+++ utils/1.py
@@ -2,19 +2,18 @@
 
 from pathlib import Path
 
-tree_str = ''
+tree_str = []
 
 def generate_tree(pathname, n=0):
     global tree_str
     if pathname.is_file():
         if pathname.name.endswith(".md"):
-           tree_str += '#' * n + pathname.name + '\n'
-    elif pathname.is_dir():
-        tree_str += '#' * n + \
-            str(pathname.relative_to(pathname.parent)) + '\\' + '\n'
+           tree_str.append({'type': 'file', 'name': '#' * n + pathname.name + '#' * n, 'path':str(pathname.resolve())})
+    elif pathname.is_dir() and pathname.name!=".git":
+        tree_str.append({'type': 'dir','name': '#' * n + str(pathname.relative_to(pathname.parent)) + '#' * n, 'path':str(pathname.resolve())})
         for cp in pathname.iterdir():
             generate_tree(cp, n + 1)
 
 if __name__ == '__main__':
-    generate_tree(Path.cwd().parent)
+    generate_tree(Path('../src'))
     print(tree_str)
