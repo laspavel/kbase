2fe7290dc1afcaa4c4029bd7777f53fcce6f01c6
laspavel <laspavel@gmail.com>
2022-06-19 19:12:30 +0300

    Final 1

--- generator.py
+++ generator.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
+from sys import builtin_module_names
 
 tree_str = []
 
 
 def generate_tree(pathname, n=0):
     global tree_str
-    global level
+    global base_path
     if pathname.is_file():
         if pathname.name.endswith(".md"):
             temp = open(str(pathname.resolve()), "r").readlines()
@@ -15,18 +16,18 @@ def generate_tree(pathname, n=0):
                 if i.startswith('# '):
                     i = i.replace('# ', '').rstrip()
                     tree_str.append(
-                        {'type': 'file', 'level': n, 'name': i, 'path': str(pathname.resolve())})
+                        {'type': 'file', 'level': n, 'name': i, 'path': str(pathname.resolve()).replace(str(base_path.as_posix())+'/', '')})
     elif pathname.is_dir() and pathname.name != ".git":
         tree_str.append({'type': 'dir', 'level': n, 'name': str(
-            pathname.relative_to(pathname.parent)), 'path': str(pathname.resolve())})
+            pathname.relative_to(pathname.parent)), 'path': str(pathname.resolve()).replace(str(base_path.as_posix())+'/', '')})
         for cp in pathname.iterdir():
             generate_tree(cp, n + 1)
 
 
 if __name__ == '__main__':
-    #    generate_tree(Path('../src'))
-    generate_tree(Path('/home/laspavel/_/kbase/src'))
-    with open('/home/laspavel/_/kbase/README.md', "w") as lf:
+    base_path = Path.cwd()
+    generate_tree(base_path)
+    with open('README.md', "w") as lf:
         lf.write("# База знаний # " + "\n")
         for data in tree_str:
             lf.write(" " * 4 * data['level'] + '* '
